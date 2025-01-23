f5752c7551bc509296a909054982c9846b1a6d1f
inspi-writer <94226358+inspi-writer001@users.noreply.github.com>
2025-01-23 14:09:26 +0100

    fixed mongoose vulnerability

--- app/controllers/user.ts
+++ app/controllers/user.ts
@@ -7,10 +7,6 @@ export const onboardUserController = async (req: Request, res: Response) => {
     const { wallet_address, email, referral_ref } = req.body;
     let new_user = await onboardUser(wallet_address, email, referral_ref);
 
-    delete new_user?._id;
-    delete new_user?.__v;
-    // delete new_user.createdAt;
-
     return serverResponse("welcome onboard Dev", new_user as Object, 200, {
       req,
       res
